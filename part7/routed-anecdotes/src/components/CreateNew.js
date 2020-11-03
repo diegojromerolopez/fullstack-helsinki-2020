11f7f4a56d1d2d57ecc30de6f12ce84c1c4c8475
Diego J <diegojromerolopez@gmail.com>
2020-11-03 20:22:43 +0100

    part7.4-7.6

--- part7/routed-anecdotes/src/components/CreateNew.js
+++ part7/routed-anecdotes/src/components/CreateNew.js
@@ -1,21 +1,29 @@
-import React, { useState } from 'react'
-import { useHistory } from "react-router-dom";
+import React from 'react'
+import { useHistory } from "react-router-dom"
+import  { useField } from '../hooks'
 
 const CreateNew = (props) => {
-    const [content, setContent] = useState('')
-    const [author, setAuthor] = useState('')
-    const [info, setInfo] = useState('')
     const history = useHistory()
+
+    const { resetContent, ...content } = useField('text')
+    const { resetAuthor, ...author}  = useField('text')
+    const { resetInfo, ...info} = useField('text')
 
     const handleSubmit = (e) => {
       e.preventDefault()
       props.addNew({
-        content,
-        author,
-        info,
+        content: content.value,
+        author: author.value,
+        info: info.value,
         votes: 0
       })
       history.push("/")
+    }
+
+    const reset = () => {
+        resetContent()
+        resetAuthor()
+        resetInfo()
     }
   
     return (
@@ -24,18 +32,19 @@ const CreateNew = (props) => {
         <form onSubmit={handleSubmit}>
           <div>
             content
-            <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+            <input {...content} />
           </div>
           <div>
             author
-            <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+            <input {...author} />
           </div>
           <div>
             url for more info
-            <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+            <input {...info} />
           </div>
           <button>create</button>
         </form>
+        <button onClick={reset}>reset</button>
       </div>
     )
   }
